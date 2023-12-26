34c8b3d02c0a90102b89734c58b71d17f175287a
--local <kuzurik797@gmail.com>
2023-12-26 05:32:33 +0000

    Purging all

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-// INDEX ROUTE
-router.get('/', (req, res) => {
-    res.redirect("/content");
-});
-
-module.exports = router;
--- run_local_mongo.sh
+++
@@ -1,6 +0,0 @@
-echo
-echo
-echo "Running Local Docker Mongo Server"
-echo "-------------------------------------------"
-echo
-docker-compose -f docker-compose-mongo.yaml up -d
